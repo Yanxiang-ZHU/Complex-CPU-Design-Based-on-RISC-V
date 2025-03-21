93bdcb9506c1e538920e772ac22ba162d4a2ef1f
Zhu Yanxiang <zhu.yx.0073@gmail.com>
2025-03-21 15:56:57 +0800

    add the verilog file

---
+++ RISC-V CPU/RISC-V CPU.srcs/sources_1/new/SPIC.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/03/21 15:48:57
+// Design Name: 
+// Module Name: SPIC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SPIC(
+
+    );
+endmodule
